8796868538d7bf87a7c2f3f60ebbf310452654f5
sontv <sontv@MacBook-Pro-cua-sontv.local>
2024-11-06 08:06:14 +0700

    list-product

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,33 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductService } from './product.service';
+import { HttpClient } from '@angular/common/http';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  constructor(private productService: ProductService){}
+  products:any= [];
+  constructor(private http: HttpClient){}
   ngOnInit(){
-    this.productService.getProducts().subscribe((data)=>{
-      console.log(data);
-      
+    this.http.get('http://localhost:3000/products').subscribe((data) => {
+      this.products = data;
     })
   }
+
+  // for(let product of products){
+  //   console.log(product);
+    
+  // }
+  // constructor(private productService: ProductService){}
+  // ngOnInit(){
+  //   this.productService.getProducts().subscribe((data)=>{
+  //     console.log(data);
+  //   })
+  // }
 }
