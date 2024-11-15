2496b4e778e46b0a478a46e7e17b639035230cfc
sontv <sontv@MacBook-Pro-cua-sontv.local>
2024-11-15 08:41:18 +0700

    add product

--- src/app/admin/product-add/product-add.component.ts
+++ src/app/admin/product-add/product-add.component.ts
@@ -1,12 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product-add',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './product-add.component.html',
   styleUrl: './product-add.component.css'
 })
 export class ProductAddComponent {
+  form:any;
+  constructor(private http:HttpClient){}
+  ngOnInit(){
+    this.form= new FormGroup({
+      name: new FormControl(''),
+      price: new FormControl(0),
+      description: new FormControl('')
+    });
+    console.log(this.form);
+    
+  }
+
+  onSubmit(){
+    console.log(this.form.value)
+  }
 
 }
