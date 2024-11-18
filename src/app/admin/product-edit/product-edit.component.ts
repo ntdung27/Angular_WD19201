f78d3e0294b76cf194527fa079335be28e0cc3bc
sontv <sontv@MacBook-Pro-cua-sontv.local>
2024-11-18 08:11:10 +0700

    edit product

--- src/app/admin/product-edit/product-edit.component.ts
+++ src/app/admin/product-edit/product-edit.component.ts
@@ -1,12 +1,50 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-edit',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './product-edit.component.html',
   styleUrl: './product-edit.component.css'
 })
 export class ProductEditComponent {
+form:any;
+  constructor(private http:HttpClient, private router:Router, private activeRoute:ActivatedRoute){}
+  ngOnInit(){
+    this.form= new FormGroup({
+      name: new FormControl(''),
+      price: new FormControl(0),
+      description: new FormControl('')
+    });
 
+    // const id = this.activeRoute.snapshot.params['id']
+    const {id} = this.activeRoute.snapshot.params
+    this.http.get(`http://localhost:3000/products/${id}`).subscribe((data) => {
+      this.form.patchValue(data)
+    })
+    
+    
+  }
+
+  onSubmit(){
+    // console.log(this.form.value);
+    const {id} = this.activeRoute.snapshot.params
+    this.http.put(`http://localhost:3000/products/${id}`, this.form.value).subscribe()
+    
+    console.log("Ban da cap nhat thanh cong");
+    
+  }
 }
+
+/*
+  B1: Tạo formGroup
+  B2: Tạo formControl
+  B3: Binding formControl và formGroup với formControlName và formGroup
+  B4: lấy id trên url
+  B5: call api lấy ra sản phẩm dựa vào id lấy được trên đường dẫn
+  B6: hiển thị dữ liệu lấy được lên form
+  B7: khi submit form thì lấy dữ liệu từ form và gửi lên server
+*/ 
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { ProductListComponent } from './admin/product-list/product-list.component';
 import { ProductAddComponent } from './admin/product-add/product-add.component';
+import { ProductEditComponent } from './admin/product-edit/product-edit.component';
 
 export const routes: Routes = [
     {path:"admin/products", component:ProductListComponent},
-    {path:"admin/products/add", component:ProductAddComponent}
+    {path:"admin/products/add", component:ProductAddComponent},
+    {path:"admin/products/:id/edit", component:ProductEditComponent}
 ];
